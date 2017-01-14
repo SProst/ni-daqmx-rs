d77ccbdd1a2edc14c21099fb99eba2cd50258011
Spencer Prost <spencer.prost@pnnl.gov>
2017-01-13 16:45:40 -0800

    Returning task_handle rather than status code.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-extern crate libc;
 mod ni_daq_mx;
 mod ni_daq_mx_gen;
 
 fn main() {
-    let result = ni_daq_mx::daq_mx_create_task("Test");
-    println!("{:?}", result);
+    let task_handle = ni_daq_mx::daq_mx_create_task("");
 }
--- src/ni_daq_mx.rs
+++ src/ni_daq_mx.rs
@@ -2,12 +2,12 @@ use std;
 use std::ffi::{CString, CStr};
 use ni_daq_mx_gen::ni_daq_mx_gen;
 
-pub fn daq_mx_create_task(task_name: &str) -> i32{
+pub fn daq_mx_create_task(task_name: &str) -> std::os::raw::c_void{
     let name = CString::new("").unwrap();
     let mut task_handle = 0 as *mut std::os::raw::c_void ;
     let status = unsafe{
         ni_daq_mx_gen::DAQmxCreateTask(name.as_ptr() as *mut i8 , &mut task_handle )
     };
     println!("{:?}", task_handle);
-    status
+    task_handle as std::os::raw::c_void;
 }
